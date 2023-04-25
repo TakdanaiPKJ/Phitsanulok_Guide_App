38e7e5b81addc723188868c0d2be459ec50f5ebf
0012 <a0012@0012s-iMac.local>
2023-04-25 09:24:47 +0700

    first commit

--- lib/userpage/userhome.dart
+++ lib/userpage/userhome.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class UserHome extends StatefulWidget {
   const UserHome({super.key});
@@ -11,9 +13,17 @@ class _UserHomeState extends State<UserHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HOME'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('HOME'),
+        ),
+        body: FlutterMap(
+          options: MapOptions(
+            center: LatLng(0, 0),
+            zoom: 10,
+          ),
+          children: const [
+            //TileLayer(options: options, mapState: mapState, stream: stream)
+          ],
+        ));
   }
 }
