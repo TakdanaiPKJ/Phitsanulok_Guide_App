209d04bcfe64e6a60fa4d6aa83f6b2d8ffb17c81
0012 <a0012@0012s-iMac.local>
2023-04-25 16:07:08 +0700

    Detail,Main Map,Map Launcher Add

--- lib/userpage/detailpage.dart
+++ lib/userpage/detailpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:maps_launcher/maps_launcher.dart';
 import 'package:test_app/homepage.dart';
 
 class DetailPage extends StatelessWidget {
@@ -9,8 +10,67 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(place.name),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.amberAccent,
+            floating: false,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.network(
+                place.pic,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              title: Text(place.name),
+              centerTitle: true,
+            ),
+            expandedHeight: 250,
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    height: 400,
+                    color: const Color.fromARGB(255, 16, 136, 200),
+                    child: Container(
+                      padding: const EdgeInsets.all(5),
+                      //margin: const EdgeInsets.all(5),
+                      child: Text(
+                        place.detail,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontStyle: FontStyle.italic,
+                            color: Color.fromARGB(255, 47, 47, 47)),
+                      ),
+                    ),
+                  )),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+                padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    height: 400,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(place.otherpic),
+                            fit: BoxFit.cover)),
+                  ),
+                )),
+          ),
+          SliverToBoxAdapter(
+            child: ElevatedButton(
+              onPressed: () => MapsLauncher.launchCoordinates(
+                  place.latt, place.lngg, '${place.name} is here'),
+              child: Text('Go to ${place.name}'),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/userpage/userhome.dart
+++ lib/userpage/userhome.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:test_app/homepage.dart';
+import 'detailpage.dart';
 
 class UserHome extends StatefulWidget {
   const UserHome({super.key});
@@ -10,20 +12,46 @@ class UserHome extends StatefulWidget {
 }
 
 class _UserHomeState extends State<UserHome> {
+  final place = listplace;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('HOME'),
-        ),
-        body: FlutterMap(
-          options: MapOptions(
-            center: LatLng(0, 0),
-            zoom: 10,
+      appBar: AppBar(
+        title: const Text('HOME'),
+      ),
+      body: FlutterMap(
+        options: MapOptions(center: LatLng(16.8239, 100.2622), zoom: 15),
+        layers: [
+          TileLayerOptions(
+            urlTemplate:
+                'https://api.mapbox.com/styles/v1/prxmz/clgvs4ush008701qudtc00tmc/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicHJ4bXoiLCJhIjoiY2xlYmIxaXd0MTdnejN2cjFxd2huZHQ0MiJ9.M_UA7jSW68pQGvr2Mbz8DQ',
+            additionalOptions: const {
+              'AccessToken':
+                  'pk.eyJ1IjoicHJ4bXoiLCJhIjoiY2xlYmIxaXd0MTdnejN2cjFxd2huZHQ0MiJ9.M_UA7jSW68pQGvr2Mbz8DQ',
+              'id': 'mapbox://styles/prxmz/clgvs4ush008701qudtc00tmc'
+            },
           ),
-          children: const [
-            //TileLayer(options: options, mapState: mapState, stream: stream)
-          ],
-        ));
+          MarkerLayerOptions(
+            markers: [
+              Marker(
+                  point: LatLng(listplace[0].latt, 100.261878),
+                  builder: (context) => IconButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) =>
+                                DetailPage(place: listplace[0]),
+                          ));
+                          //Navigator.pushNamed(context, 'watyai');
+                        },
+                        icon: const Icon(Icons.circle),
+                        color: Colors.red,
+                        iconSize: 10.0,
+                      ))
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
