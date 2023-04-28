cc2ebf0d5c95ee02b2486a0512a58aa88e99ecd9
0012 <a0012@0012s-iMac.local>
2023-04-28 15:21:55 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        fontFamily: 'ChakraPetch',
-      ),
+          //fontFamily: 'ChakraPetch',
+          ),
       initialRoute: '/',
       routes: {
         '/': (context) => const HomePage(),
